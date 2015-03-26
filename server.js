a9fa8eb1a7d03681b535c38385b8607369026929
Alfredo López Moltó <xgalen@gmail.com>
2015-03-26 16:49:37 +0100

    Nombre correcto del servidor

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var http = require('http'),
     app = require('./app');
 if (process.env.NODE_ENV === 'production') {
     http.createServer(app).listen(3001, function () {
-        console.log('Logiters %s module listening on port %s', process.env.NODE_ENV, 3001);
+        console.log('Damco %s module listening on port %s', process.env.NODE_ENV, 3001);
     });
 } else {
     http.createServer(app).listen(app.get('port'), function () {
-        console.log('Logiters %s module listening on port %s', process.env.NODE_ENV, app.get('port'));
+        console.log('Damco %s module listening on port %s', process.env.NODE_ENV, app.get('port'));
     });
 }
 process.on('uncaughtException', function (err) {
